6ea7992c3ea906c555e56ca2418fd67b6934e299
asyncci <myrzabekovadik9@gmail.com>
2023-07-07 00:05:29 +0600

    l

--- main/urls.py
+++ main/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("account.urls")),
-    path('', include('order.urls'))
+    path('', include('order.urls')),
+    path('', include(''))
 ]
